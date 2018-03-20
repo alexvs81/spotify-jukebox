f04016a931f2f0818deb2cd177708c4a4d434b93
Alex Sørensen <alex.vejlgaard@live.dk>
2018-03-20 23:41:11 +0100

    Creatign the playlist component, and adding the fakedata to it

--- src/App.js
+++ src/App.js
@@ -122,14 +122,15 @@ class Filter extends Component {
 
 class Playlist extends Component {
   render() {
+    let playlist = this.props.playlist
     return (
       <div style={{...defaultStyle, width: '20%', display: 'inline-block'}}>
         <img src="" alt="" />
-        <h3>Playlist Name</h3>
+        <h3>{playlist.name}</h3>
         <ul>
-        <li>Song 1</li>
-        <li>Song 2</li>
-        <li>Song 3</li>
+        {playlist.songs.map(song =>
+          <li>{song.name}</li>
+        )}
         </ul>
       </div>
     );
@@ -155,10 +156,11 @@ class App extends Component {
         <PlaylistCounter playlists={this.state.serverData.user.playlists} />
         <HoursCounter playlists={this.state.serverData.user.playlists} />
         <Filter />
-        <Playlist />
-        <Playlist />
-        <Playlist />
-        <Playlist />
+        {
+          this.state.serverData.user.playlists.map(playlist =>
+            <Playlist playlist={playlist} />
+          )
+        }
       </div> : 'Loading...'
       }
       </div>
